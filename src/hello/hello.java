bcc5591a1a0ef5daff134564f9dd6ec989ce7bd9
vidyarajamanickam <coolvidyaraj@gmail.com>
2016-02-16 17:19:30 +0530

    hellofinal

---
+++ src/hello/hello.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class hello {
+
+	public String helloworld(){
+		System.out.print("hello world");
+		return "hello";
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+	}
+}
